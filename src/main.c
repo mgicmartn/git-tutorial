1ff1232c495c0dc08b40fae1cc600d76e076273b
mgicmartn <nitram8@t-online.de>
2020-04-28 14:47:12 +0200

    Coding Challenge 2

--- src/main.c
+++ src/main.c
@@ -1,6 +1,50 @@
+/*
+ ============================================================================
+ Name        : ESPL_CodeChallenge2
+ Author      : Martin Zimmermann
+ Version     :
+ Copyright   : Your copyright notice
+ Description : User Interface for num_to_words convertion.
+ ============================================================================
+ */
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-	printf("Hello ESPL!!");
+
+	char *words;
+
+	int num;
+	int quit = 0;
+	char response[1] = "y";
+
+
+	do{
+		printf("Please enter a number.\n");
+		fflush(stdout);
+		scanf("%d", &num);
+//		printf("%i\n",num);
+
+		words = num_to_words(num);
+
+		printf("%s",words);
+
+		printf("\nDo you want to enter another number? y/n\n");
+		fflush(stdout);
+		scanf(" %s", response);
+//		printf("%s\n", response);
+
+		if(*response == 'n'){
+			printf("Quit.");
+			quit=1;
+		}
+		else if(*response == 'y') printf("Have fun!\n");
+		else {
+			printf("\nWrong input. Quit.");
+			quit=1;
+		}
+
+	}while(quit==0);
+
+	return 0;
 }
